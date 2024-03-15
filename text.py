a07d8d47e0ebad31c6c8f4e2f2c1025ea2d0387b
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-16 03:26:03 +0530

    Top 5 Purchased Items and their Quantities

--- text.py
+++ text.py
@@ -24,6 +24,27 @@ for purchase in info['purchase_history']:
 # Most Purchased top 3 Items
 top_purchased_items = pd.Series(items).value_counts().nlargest(3)
 
+# Generated total revenue
+total_revenue = sum([float(p.split(':')[1]) for purchase in info['purchase_history'] for p in purchase.split(';')])
+
+# Average Purchase Per Customer
+avg_purchase_per_customer = total_revenue / customers_num
+
+# Identify Customers who Spent Above $100
+high_spending_customers = info[info['purchase_history'].apply(lambda x: sum(float(p.split(':')[1]) for p in x.split(';'))) > 100]['customer_id'].tolist()
+
+# Find Customers who Purchased a Specific Item
+customers_of_specific_item = info[info['purchase_history'].str.contains('Product X')]['customer_id'].tolist()
+
+# Group Customers by Location & Calculate Total Spending per Location
+total_spending_per_location = info.groupby('location')['purchase_history'].apply(lambda x: sum(float(p.split(':')[1]) for purchase in x.split(';')))
+
+#Top 5 Purchased Items and their Quantities
+import matplotlib.pyplot as plt
+
+top_purchased_items.plot(kind='bar', xlabel='Item', ylabel='Quantity', title='Top 5 Purchased Items')
+plt.show()
+
 
 
 
