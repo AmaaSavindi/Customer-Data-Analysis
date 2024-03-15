f7f6ebb222915234efcdc64a5f996877f19185cb
AmaaSavindi <amaa.20220020@iit.ac.lk>
2024-03-16 03:10:06 +0530

    Importing libraries and CSV file

--- text.py
+++ text.py
@@ -1,1 +1,6 @@
+#Importing the libraries to read CSV and data manipulation
+import pandas as pd
+
+# Read the CSV file
+info = pd.read_csv('CSV File for Task 02  (Medium) - CSV File for Task 02  (Medium).csv')
 
